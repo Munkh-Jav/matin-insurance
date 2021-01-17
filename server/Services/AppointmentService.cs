8f1b171cdd7aedf14aa9f5111524836a66cddaba
Anis Brachemi <anisbrachemi@hotmail.com>
2021-01-17 15:03:13 -0500

    BHAY

--- server/Services/AppointmentService.cs
+++ server/Services/AppointmentService.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using MongoDB.Driver;
 using server.Models;
 using MongoDB.Bson;
+using System;
 
 namespace server.Services
 {
@@ -9,17 +10,27 @@ namespace server.Services
     public class AppointmentService
     {
         private readonly IMongoCollection<Appointment> _Appointments;
+        private readonly IMongoCollection<Stats> _Stats;
 
         public AppointmentService(IDatabaseSettings settings)
         {
             var client = new MongoClient(settings.ConnectionString);
             var database = client.GetDatabase(settings.DatabaseName);
             _Appointments = database.GetCollection<Appointment>("Appointments");
+            _Stats = database.GetCollection<Stats>("Stats");
         }
  
         public Appointment Create(Appointment Appointment)
         {
             _Appointments.InsertOne(Appointment);
+            Stats stat = _Stats.Find(sub=>sub.type == "active-appointments").SingleOrDefault();
+            if(DateTime.Now.AddDays(-7).CompareTo(stat.last_update) > 0){
+                stat.previous = stat.value;
+                stat.value = 0;
+                stat.last_update = DateTime.Now;
+            }
+            stat.value++;
+            _Stats.ReplaceOne(sub => sub.type == "active-appointments", stat);
             return Appointment;
         }
 
@@ -29,8 +40,20 @@ namespace server.Services
         public Appointment Get(string id) =>
             _Appointments.Find(sub=>sub.Id == id).SingleOrDefault();
 
-        public void Update(string id, Appointment Appointment) =>
+        public void Update(string id, Appointment Appointment) {
+            if(Appointment.status == 2){
+                Stats stat = _Stats.Find(sub=>sub.type == "active-appointments").SingleOrDefault();
+                stat.value--;
+                if(DateTime.Now.AddDays(-7).CompareTo(stat.last_update) > 0){
+                    stat.previous = stat.value;
+                    stat.value = 0;
+                    stat.last_update = DateTime.Now.AddDays(-7);
+                }
+                _Stats.ReplaceOne(sub => sub.type == "active-appointments", stat);
+            }
             _Appointments.ReplaceOne(sub => sub.Id == id, Appointment);
+        }
+            
 
         public void Delete(string id) =>
             _Appointments.DeleteOne(sub => sub.Id == id);
--- server/Services/CommentService.cs
+++ server/Services/CommentService.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using MongoDB.Driver;
 using server.Models;
+using System;
 
 namespace server.Services
 {
@@ -8,17 +9,25 @@ namespace server.Services
     public class CommentService
     {
         private readonly IMongoCollection<Comment> _Comments;
+        private readonly IMongoCollection<Stats> _Stats;
 
         public CommentService(IDatabaseSettings settings)
         {
             var client = new MongoClient(settings.ConnectionString);
             var database = client.GetDatabase(settings.DatabaseName);
             _Comments = database.GetCollection<Comment>("Comments");
+            _Stats = database.GetCollection<Stats>("Stats");
         }
  
         public Comment Create(Comment Comment)
         {
             _Comments.InsertOne(Comment);
+            Stats stat = _Stats.Find(sub=>sub.type == "comments").SingleOrDefault();
+            if(DateTime.Now.AddDays(-7).CompareTo(stat.last_update) < 0){
+                stat.last_update = DateTime.Now;
+            }
+            stat.value++;
+            _Stats.ReplaceOne(sub => sub.type == "comments", stat);
             return Comment;
         }
 
@@ -32,10 +41,29 @@ namespace server.Services
         public Comment Get(string id) =>
             _Comments.Find(sub=>sub.Id == id).SingleOrDefault();
 
-        public void Update(string id, Comment Comment) =>
+        public void Update(string id, Comment Comment) {
+            if(Comment.status != 0){
+                Stats stat = _Stats.Find(sub=>sub.type == "comments").SingleOrDefault();
+                stat.value--;
+                if(DateTime.Now.AddDays(-7).CompareTo(stat.last_update) < 0){
+                    stat.last_update = DateTime.Now;
+                }
+                _Stats.ReplaceOne(sub => sub.type == "comments", stat);
+            }
             _Comments.ReplaceOne(sub => sub.Id == id, Comment);
+        }
 
-        public void Delete(string id) =>
+        public void Delete(string id) {
+            Comment comment = Get(id);
+            if(comment.status == 0){
+                Stats stat = _Stats.Find(sub=>sub.type == "comments").SingleOrDefault();
+                stat.value--;
+                if(DateTime.Now.AddDays(-7).CompareTo(stat.last_update) < 0){
+                    stat.last_update = DateTime.Now;
+                }
+                _Stats.ReplaceOne(sub => sub.type == "comments", stat);
+            }
             _Comments.DeleteOne(sub => sub.Id == id);
+        }
     }
 }
